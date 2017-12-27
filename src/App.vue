62d9ee86de006b81fd24ce82e61b41895bbac8d8
leonsuxgit config --global user.email leonsux@163.com <leonsux@163.com>
2017-12-27 17:33:50 +0800

    add yo and vuex

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+    <div class="tab-bar"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+#app {
+  height: 100%;
+  
+  .tab-bar{
+
+  }
+}
+</style>
---
+++ src/components/browse/AppBrowse.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <header>浏览</header>
+    <div class="search"></div>
+    <div class="experience"></div>
+    <div class="type"></div>
+    <div class="bottom-bar"></div>
+  </div>
+</template>
+
+<script>
+  
+  export default {
+    name: 'app-browse'
+  }
+</script>
+
+<style lang="scss">
+  
+</style>
